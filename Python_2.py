4b45a4e8d6a467ede840a181072a75d2c9c9fd6b
legendretheo <132549112+legendretheo@users.noreply.github.com>
2023-05-04 11:29:00 +0200

    Add files via upload

---
+++ Python_2.py
@@ -0,0 +1,12 @@
+Zahl1= int(input("Gib Zahl ein:"))
+#if [Bedigung] :
+      
+while Zahl1 ==0:
+    print("Erste Zahl darf nicht Null sein")
+    Zahl1 = int(input("Gib nochmal eine zahl ein:") )
+else:           
+    Zahl2 = int(input ("Gib noch eine Zahl ein:"))
+    print ("Summe der zahlen ist:" ,Zahl1 + Zahl2)
+    print ("Differenz der zahlen ist:",Zahl2 - Zahl1)
+    print("Zahlen multipliziert ergeben:", Zahl1 * Zahl2)
+    print ("Zahlen dividiert ergeben:", Zahl2/ Zahl1)

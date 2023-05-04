6b8588aee1e5a1106f89c0984b6913b3c8c52b63
legendretheo <132549112+legendretheo@users.noreply.github.com>
2023-05-04 10:31:03 +0200

    Add files via upload

---
+++ Python_1.py
@@ -0,0 +1,14 @@
+a=3
+b=4
+c=a+b
+print(c)
+Zahl1 =int(input ("Gib eine Zahl ein:")) 
+Zahl2 = int(input ("Gib noch eine Zahl ein:"))
+print ("Summe der zahlen ist:" ,Zahl1 + Zahl2)
+print ("Differenz der zahlen ist:",Zahl2 - Zahl1)
+print("Zahlen multipliziert ergeben:", Zahl1 * Zahl2)
+print ("Zahlen dividiert ergeben:", Zahl2/ Zahl1)
+
+
+
+
